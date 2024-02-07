92766855b3f05c8b41fed4700143eae87c97bc2b
maskenissen1 <159002509+maskenissen1@users.noreply.github.com>
2024-02-07 10:58:39 +0200

    сhanged js

--- game.js
+++ game.js
@@ -54,7 +54,24 @@ function create ()
         frameRate: 10,
         repeat: -1
     });
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 }
+
+function update ()
+{
+}
+
 
 function update ()
 {
