3eb20a11d8498be60336fbff097c1b745b40f4f9
maskenissen1 <159002509+maskenissen1@users.noreply.github.com>
2024-02-07 10:54:18 +0200

    changed js

--- game.js
+++ game.js
@@ -2,12 +2,21 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
     }
 };
+
+var platforms;
 
 var game = new Phaser.Game(config);
 
@@ -20,9 +29,19 @@ function preload ()
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
+var platforms;
+
 function create ()
 {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
